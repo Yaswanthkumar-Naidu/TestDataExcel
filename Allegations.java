d68840a708d3c7797baf14a5221b47372704a967
Yaswanthkumar-Naidu <Yaswanthkumar.Naidu@otsi.ca.gov>
2024-12-20 08:36:41 +0530

    Update Allegations.java

--- Allegations.java
+++ Allegations.java
@@ -12,8 +12,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
